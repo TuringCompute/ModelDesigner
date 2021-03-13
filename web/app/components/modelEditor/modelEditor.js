92145e68498af6a2f5d4bb895236b566a39694b6
TuringHowey <huoyihy@hotmail.com>
2021-03-13 08:44:35 -0800

    add view menu to modelEditor.js top

--- web/app/components/modelEditor/modelEditor.js
+++ web/app/components/modelEditor/modelEditor.js
@@ -3,11 +3,14 @@ import {DataStore} from "../../../external/turingDiv.js/lib/dataStore.js"
 import {Format} from "../../../external/turingDiv.js/lib/format.js"
 import {ModelPropEditor} from "../property/property.js"
 import {ModelViewer} from "../viewer/viewer.js"
+import {MenuReg} from "../../../external/turingDiv.js/component/menuReg/menuReg.js"
 
 class ModelEditor extends DivEle{
-    static childProperty = "ModelEditorProperty"
-    static childViewer = "ModelEditorViewer"
-
+    static Keys = Object.freeze({
+        "childProperty": "ModelEditorProperty",
+        "childViewer": "ModelEditorViewer",
+        "menuView": "menuModelDesignerView"
+    })
 
     constructor(props){
         super(props)
@@ -19,25 +22,30 @@ class ModelEditor extends DivEle{
         this.layout = this.dataStore.newData(this.layoutDataId, DataStore.subscriber(this.layoutDataId, this.handleEvent))
         this.layout.hidden = []
         this.init_children()
+        let menuReg = MenuReg.GetMenu()
+        this.menuView = new MenuView({
+            "parentId": menuReg.id,
+            "childId": ModelEditor.Keys.menuView
+        })
     }
 
     init_children(){
         new ModelPropEditor({
-            "childId": ModelEditor.childProperty,
+            "childId": ModelEditor.Keys.childProperty,
             "parentId": this.id
         })
         new ModelViewer({
-            "childId": ModelEditor.childViewer,
+            "childId": ModelEditor.Keys.childViewer,
             "parentId": this.id
         })
     }
 
     outputHTML(){
-        let cProp = this.children.getValue(ModelEditor.childProperty)
-        let cViewer = this.children.getValue(ModelEditor.childViewer)
+        let cProp = this.children.getValue(ModelEditor.Keys.childProperty)
+        let cViewer = this.children.getValue(ModelEditor.Keys.childViewer)
         let htmlList = []
         htmlList.push("<table><tr>")
-        if(this.layout.hidden.indexOf(ModelEditor.childProperty) == -1){
+        if(this.layout.hidden.indexOf(ModelEditor.Keys.childProperty) == -1){
             htmlList.push("<td>")
             let propHtml = cProp.node.outputHTML()
             Format.applyIndent(propHtml)
@@ -55,5 +63,27 @@ class ModelEditor extends DivEle{
     }
 
 }
+
+class MenuView extends DivEle{
+    constructor(props){
+        super(props)
+    }
+
+    outputHTML(){
+        let htmlList = [
+            "<table border=1>",
+            "  <tr>",
+            "    <td onClick='alert(1)'>line 1</td>",
+            "  </tr>",
+            "  <tr>",
+            "    <td onClick='alert(2)'>line 2</td>",
+            "  </tr>",
+            "</table>"
+        ]
+        return htmlList
+    }
+
+}
+
 
 export {ModelEditor}
--- web/app/main.js
+++ web/app/main.js
@@ -1,11 +1,13 @@
-import {MouseState} from "../external/turingDiv.js/lib/mouse.js"
 import {ModelEditor} from "../app/components/modelEditor/modelEditor.js"
 import { DataStore } from "../external/turingDiv.js/lib/dataStore.js"
+import { MenuReg } from "../external/turingDiv.js/component/menuReg/menuReg.js"
 
 window.main = function main(){
     //Utilize the MouseState class from mouse library for mouse event
-    MouseState.start()
     let store = new DataStore()
+    new MenuReg({
+        "divId": "menuModelDesignerView"
+    })
     let modelEditor = new ModelEditor({
         "dataStore": store,
         "divId": "main"
